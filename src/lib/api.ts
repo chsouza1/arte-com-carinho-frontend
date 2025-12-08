d4722cb47975ecfeb0bfb472b450fc4ae6f353d7
carlos <carloshenrique.souza88@hotmail.com>
2025-12-08 15:08:59 -0300

    update public Api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getSession } from "@/lib/auth";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "https://divine-avis-arte-com-carinho-5a6aab3a.koyeb.app/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "https://divine-avis-arte-com-carinho-5a6aab3a.koyeb.app/api",
 });
 
 export function setAuthToken(token?: string | null) {
